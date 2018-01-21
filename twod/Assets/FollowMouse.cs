2033e9dcab3a0d0e57e7e2b1e0f57a92518c8ea7
Chris Underhill <cpu22@cam.ac.uk>
2018-01-21 04:43:11 +0000

    Added the entire game section of the project

---
+++ twod/Assets/FollowMouse.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var sens = 100;
+        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(pos);
+        var dir = (pos - transform.position).normalized;
+        GetComponent<Rigidbody2D>().AddForce(dir*sens);
+
+
+        //GetComponent<Rigidbody>().velocity = ((transform.right * mouseMovement.x) + (transform.forward * mouseMovement.y)) / Time.deltaTime;
+    }
+
+    /*
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Head")
+        {
+            Debug.Log("AAAAAGGGGGHHHHHHH");
+            var damage =  GetComponent<Rigidbody2D>().velocity.magnitude * GetComponent<Rigidbody2D>().mass / Time.deltaTime;
+            Debug.Log(damage);
+        }
+    }
+    */
+}
---
+++ twod/Assets/TakeDamage.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakeDamage : MonoBehaviour {
+
+    [Range(0,1000)]
+    public int startingHealth = 100;                            // The amount of health the player starts the game with.
+    public int currentHealth;                                   // The current health the player has.
+    public RectTransform healthBar;                                 // Reference to the UI's health bar.
+    private int iframescount;
+    public int iframeslen;
+    private bool invincible;
+
+    void Start () {
+        currentHealth = startingHealth;
+        iframescount = 0;
+        invincible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (invincible)
+        {
+            iframescount++;
+
+            if (iframescount >= iframeslen)
+            {
+                invincible = false;
+                iframescount = 0;
+            }
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.layer == 8 || col.gameObject.layer == 12)
+        {
+            if (!invincible)
+            {
+                //Debug.Log("AAAAAGGGGGHHHHHHH");
+                var damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * col.gameObject.GetComponent<Rigidbody2D>().mass / Time.deltaTime;
+                //Debug.Log(damage);
+
+                currentHealth -= (int)damage;
+
+                if (currentHealth < 0)
+                {
+                    GetComponent<FixedJoint2D>().enabled = false;
+                    GetComponent<Rigidbody2D>().AddForce((transform.position - col.gameObject.transform.position).normalized * col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 300);
+                }
+
+
+                healthBar.sizeDelta = new Vector2((float)currentHealth / (float)startingHealth * 100, healthBar.sizeDelta.y);
+
+                invincible = true;
+            }
+        }
+    }
+}
